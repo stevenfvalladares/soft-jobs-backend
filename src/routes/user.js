d92ba0d90cc9221a53946f03d600e66c46eca3b9
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-07 11:46:25 -0300

    Routes are created to query users

---
+++ src/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const { createUsers, getUsers } = require('../controllers/user')
+const { checkRequest } = require('../middlewares/checkRequest')
+const { validation } = require('../middlewares/authenticated')
+
+router.post('/users', checkRequest, createUsers);
+router.get('/users', checkRequest, validation ,getUsers);
