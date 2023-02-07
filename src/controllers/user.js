9e31efb43c35a9c8d9b2c7104c00f9a4f0e14e4b
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-07 11:34:30 -0300

    Added controller for users

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,1 +1,29 @@
+const { encrypt } = require("../helpers/encrypting");
+const { decode } = require("../middlewares/authenticated");
+const { createUser, getUser } = require("../models/user");
 
+const createUsers = async (req, res) => {
+  try {
+    const payload = req.body;
+    const passwordEncrypt = encrypt(req.body.password);
+    const payloadEncrypt = { ...payload, ...{ password: passwordEncrypt } };
+    result = await createUser(payloadEncrypt);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(error.code || 500).send(error);
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const tokenJWT = req.header("Authorization");
+    const token = decode(tokenJWT);
+    const { email } = token;
+    result = await getUser(email);
+    res.send(result);
+  } catch (error) {
+    res.status(error.code || 500).send(error);
+  }
+};
+
+module.exports = { createUsers, getUsers };
